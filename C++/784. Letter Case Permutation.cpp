f02044c83d2a6eabfcbed312b61fad461920ef89
[Tencen] <[Tencen_hong@outlook.com]>
2019-05-04 08:21:43 +0800

    Update 784. Letter Case Permutation.cpp

--- C++/784. Letter Case Permutation.cpp
+++ C++/784. Letter Case Permutation.cpp
@@ -1,20 +1,25 @@
-class Solution {
+class Solution
+{
 public:
     vector<string> res;
-    void help(string s,int index){
-        if(index==s.size()){
+    void help(string s, int index)
+    {
+        if (index == s.size())
+        {
             res.push_back(s);
             return;
         }
-        help(s,index+1);
-        if(isalpha(s[index])){
-            s[index] ^=32;
-            help(s,index+1);
+        help(s, index + 1);
+        if (isalpha(s[index]))
+        {
+            s[index] ^= 32;
+            help(s, index + 1);
         }
     }
-    vector<string> letterCasePermutation(string S) {
-     
-        help(S,0);
+    vector<string> letterCasePermutation(string S)
+    {
+
+        help(S, 0);
         return res;
     }
 };
